359b1ec026e57f2cbb00f386b30288bcdb3c0cc1
sinianzhong <166645257+sinianzhong@users.noreply.github.com>
2025-02-28 16:08:29 -0700

    song.java

---
+++ song.java
@@ -0,0 +1,29 @@
+package la1;
+
+public class song {
+	private final String title;
+	private final String artist;
+	private final String album;
+
+	public song(String title, String artist, String album) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public String toString() {
+		return title + " - " + artist + " (Album: " + album + ")";
+	}
+}
